3653b7efa8354df4a16afe439c91fccc6bdd12f3
Santosh Kumar <kasantosh@yahoo.com>
2019-12-18 11:31:10 -0500

    fixed minor input issues - strong to float for the value

--- script/app.js
+++ script/app.js
@@ -79,7 +79,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMStrings.inputType).value, //Will either be 'inc' or 'exp', see markup
                 description: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMStrings.inputValue).value)
 
             };
 
@@ -149,7 +149,15 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
     };
 
+    var updateBudget = function () {
 
+        // 1. Calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+
+    }
 
     var ctrlAddItem = function () {
 
@@ -167,9 +175,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Clear the fields
         UICtrl.clearFields();
 
-        // 5. Calculate the budget
-
-        // 6. Display the budget on the UI
+        // 5. Calculate and update budget
+        updateBudget();
 
     };
 
