1c4c2ad6ca330def562bf6c163a28270e7c936bf
Santosh Kumar <kasantosh@yahoo.com>
2019-12-27 20:01:49 -0500

    App is fully functional, final minor clean ups

--- script/app.js
+++ script/app.js
@@ -159,7 +159,8 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 
     };
 
@@ -237,7 +238,25 @@ var UIController = (function () {
 
         },
 
-        displayPercentages: function () {
+        displayPercentages: function (percentages) {
+
+            var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
+
+            var nodeListforEach = function (list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListforEach(fields, function (current, index) {
+
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '--';
+                }
+
+            });
 
         },
 
@@ -289,7 +308,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
 
     }
 
